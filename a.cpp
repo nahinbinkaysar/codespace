08c3ac79597f0d5fa7ca72ec09152ff168a4a385
Nahin Bin Kaysar <nahinbinkaysar@gmail.com>
2023-01-31 19:36:43 +0000

    modified:   a.cpp

--- a.cpp
+++ a.cpp
@@ -1,1 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) x.begin(),x.end()
+#define ll long long
+#define gap ' '
+#define endl '\n'
+#define vi vector<int>
+#define debug cout << "DEBUG: " << 
 
+void solve()
+{
+    
+}
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while(t--) {
+    	solve();
+    }
+    return 0;
+}
