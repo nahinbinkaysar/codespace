2e2ab0e3eef003133d2c4db001d796d71ffa553e
Nahin Bin Kaysar <nahinbinkaysar@gmail.com>
2023-02-04 05:52:51 +0000

    modified:   app.js 	modified:   sample.txt

--- app.js
+++ app.js
@@ -15,12 +15,46 @@ app.get('/', (req, res) => {
           <title>My Node.js App</title>
         </head>
         <body>
-          <h1>Contents of sample.txt</h1>
           <pre>${data}</pre>
-        </body>
-      </html>
+          <button id="myButton">Press Me</button>
+          <script>
+          const button = document.getElementById("myButton");
+          button.addEventListener("click", () => {
+            fetch("/button-press")
+              .then(response => response.text())
+              .then(data => {
+                console.log(data);
+              });
+          });
+        </script>
+      </body>
+    </html>
     `);
   });
+});
+
+app.get('/button-press', (req, res) => {
+  console.log("Button press detected.");
+  res.send("Button press detected.");
+
+  fs.readFile('sample.txt', 'utf-8', function (err, data) {
+    if (err) throw err;
+
+    console.log(typeof (data)); // string
+
+    console.log(data); // string
+
+  });
+
+  fs.writeFile('sample.txt', 'asdsadasf', 'utf-8', (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    console.log('Text appended to file');
+  });
+  fetch("/");
 });
 
 app.listen(port, () => {
